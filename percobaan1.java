1842263f3cb75ee7f158187d068bb99a7db4934a
chaidirali51017030 <48231528+chaidirali51017030@users.noreply.github.com>
2019-09-11 21:37:55 -0700

    Add files via upload

---
+++ percobaan1.java
@@ -0,0 +1,21 @@
+public class percobaan1 {
+	public static void main(String[] args) {
+		// tipe data primitif
+		long data1 = 767226531;
+		int data2 = 2235641;
+		short data3 = 714;
+		byte data4 = 34;
+		float data6 = (float) 1.733; // tipe data pecahan
+		double data5 = 4.967; // tipe data pecahan
+		char data7 = 'C';
+		boolean data8 = true;
+		System.out.println("Nilai long 	 : "+ data1);
+		System.out.println("Nilai int  	 : "+ data2);
+		System.out.println("Nilai short	 : "+ data3);
+		System.out.println("Nilai byte 	 : "+ data4);
+		System.out.println("Nilai double : "+ data5);
+		System.out.println("Nilai float  : "+ data6);
+		System.out.println("Nilai char	 : "+ data7);
+		System.out.println("Nilai boolean: "+ data8);
+	}
+}
---
+++ testjava.java
@@ -0,0 +1,5 @@
+public class testjava{
+	public static void main(String [] args){
+		System.out.println("dhidi \n" + "sangat" + "tampan");
+	}
+}
